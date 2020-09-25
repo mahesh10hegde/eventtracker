e2c1cfa7c639cad12873c3649cf4c2a232c7052a
Mahesh Hegde <mahesh10.hegde@gmail.com>
2020-09-25 21:33:04 +0530

    event creation form

--- src/pages/CreateEvent.js
+++ src/pages/CreateEvent.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Header from '../components/header/Header';
-
+import '../../src/assets/css/eventform.css';
 class CreateEvent extends Component{
   
   render(){
@@ -8,7 +8,30 @@ class CreateEvent extends Component{
         
         <div className="event-creation-container">
            <Header></Header>
-           <div className="event-creation-form">Create event</div>
+           <div className="event-creation-form">
+             <form id="eventCreationForm">
+                <div className="label-wrap">
+                  <label>Meeting date</label>
+                  <input type="date"></input>
+                </div>
+                <div className="label-wrap">
+                  <label>Meeting name</label>
+                  <input type="text"></input>
+                </div>
+                <div className="label-wrap">
+                  <label>Meeting description</label>
+                  <textarea type="date"></textarea>
+                </div>
+                <div className="label-wrap">
+                  <label>Meeting Attendees</label>
+                  <input type="email"></input>
+                </div>
+                <div className="button-wrap">
+                  <button className="submit-btn">Submit</button>
+                </div>
+             </form>
+             
+           </div>
         </div>
     );
   }
